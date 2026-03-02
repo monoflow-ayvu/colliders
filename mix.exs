d90f90f310f702066ab22903a79329df25099004
Vinicius <vinicius.alexandre68@gmail.com>
2026-03-02 14:31:32 -0300

    Add credo dependency

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Colliders.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -21,8 +22,18 @@ defmodule Colliders.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp aliases do
+    [
+      check: [
+        "format --check-formatted",
+        "deps.unlock --check-unused",
+        "credo suggest --strict --all"
+      ]
     ]
   end
 end
