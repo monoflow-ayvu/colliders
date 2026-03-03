897d78701773c01ff3c2550c9c3e8db995e41b09
Vinicius <vinicius.alexandre68@gmail.com>
2026-03-03 10:25:42 -0300

    add ex_doc lib

--- mix.exs
+++ mix.exs
@@ -1,14 +1,20 @@
 defmodule Colliders.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/monoflow-ayvu/colliders"
+  @version "0.1.1"
+
   def project do
     [
       app: :colliders,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases()
+      aliases: aliases(),
+      docs: docs(),
+      package: package(),
+      description: "An Elixir library for detecting whether a bbox overlaps a polygon"
     ]
   end
 
@@ -24,7 +30,8 @@ defmodule Colliders.MixProject do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:geo, "~> 4.1"},
-      {:geo_turf, "~> 0.3.1"}
+      {:geo_turf, "~> 0.3.1"},
+      {:ex_doc, "~> 0.40", only: :dev, runtime: false}
     ]
   end
 
@@ -36,5 +43,31 @@ defmodule Colliders.MixProject do
         "credo suggest --strict --all"
       ]
     ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      name: "Colliders",
+      suorce_ref: "v#{@version}",
+      source_url: @source_url,
+      extras: extras()
+    ]
+  end
+
+  defp extras do
+    [
+      "README.md": [
+        title: "Readme"
+      ]
+    ]
+  end
+
+  defp package do
+    %{
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url},
+      maintaners: ["Vinicius Costa", "Fernando Mumbach"]
+    }
   end
 end
